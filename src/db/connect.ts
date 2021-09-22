66983060679f3f66b839e6401be861dfc6def7ff
smartinsdev <smartinsdev>
2021-09-23 01:52:02 +0200

    ConnectOptions remove

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,4 +1,4 @@
-import mongoose, { MongooseOptions } from "mongoose";
+import mongoose from "mongoose";
 
 export const connectDB = (url: string) => {
   return mongoose.connect(url, () => console.log("Mongo Connect"));
