552b79033ccff65953d5c9923d28b4c608b54cc0
smartinsdev <smartinsdev>
2021-09-23 02:54:43 +0200

    move not-found.ts to into route

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { todoRouter } from "./routers/todo";
 import { connectDB } from "./db/connect";
 import { errorHandlerMiddleware } from "./middleware/error-handler";
-import { notFound } from "./middleware/not-found";
+import { notFound } from "./routers/not-found";
 
 require("dotenv").config();
 
@@ -10,9 +10,10 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(errorHandlerMiddleware);
-app.use(notFound);
 //routers
 app.use("/api/v1/todo", todoRouter);
+app.use(notFound);
+
 const port = process.env.PORT || 3000;
 
 const startApp = async () => {
